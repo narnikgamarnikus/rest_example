f84f1a7fe44377e8d211bdad924e0550c59d1a2e
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 01:30:28 +0400

    Created blank apps app

---
+++ rest_example/apps/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "Vladimir Myshkovski"
+__credits__ = ["Vladimir Myshkovski"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Vladimir Myshkovski"
+__email__ = "vladimirmyshkovski@gmail.com"
+__status__ = "Development"
+
