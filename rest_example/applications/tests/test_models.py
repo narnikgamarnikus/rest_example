4289c9d47e8cf3a4245a0b63e37314f7b961f9b2
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 02:10:09 +0400

    test_generate_new_api_key in TestApplicationModel passed

--- rest_example/applications/tests/test_models.py
+++ rest_example/applications/tests/test_models.py
@@ -5,4 +5,4 @@ pytestmark = pytest.mark.django_db
 
 class TestApplicationModel:
     def test_generate_new_api_key(self, application):
-        assert len(application.api_key) == 36
+        assert len(application.api_key) == 32
