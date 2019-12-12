8321647bf274924867388065c540e02f7eb85220
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 02:11:52 +0400

    Added test_api_key_exists to TestApplicationModel

--- rest_example/applications/tests/test_models.py
+++ rest_example/applications/tests/test_models.py
@@ -4,5 +4,9 @@ pytestmark = pytest.mark.django_db
 
 
 class TestApplicationModel:
+    
     def test_generate_new_api_key(self, application):
         assert len(application.api_key) == 32
+
+    def test_api_key_exists(self, application):
+        assert application.api_key_exists(api_key_exists.api_key)
