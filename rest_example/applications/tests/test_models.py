1acde5df352d6594aade096296a5a6d1e86ffa7d
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 02:18:46 +0400

    Added test_set_api_key to TestApplicationModel

--- rest_example/applications/tests/test_models.py
+++ rest_example/applications/tests/test_models.py
@@ -10,3 +10,8 @@ class TestApplicationModel:
 
     def test_api_key_exists(self, application):
         assert application.__class__.api_key_exists(application.api_key)
+
+    def test_set_api_key(self, application):
+        old_api_key = application.api_key
+        application.set_new_api_key()
+        assert old_api_key != application.api_key
