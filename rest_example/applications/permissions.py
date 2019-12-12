ec9ec33650a44a24b7f8e35f812bbbffa00c5f54
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 03:29:36 +0400

    Added test action to ApplicationViewSet

--- rest_example/applications/permissions.py
+++ rest_example/applications/permissions.py
@@ -3,9 +3,9 @@ from rest_framework.authentication import get_authorization_header
 
 
 class ApplicationAPIKeyPermission(BasePermission):
-    message = 'Requests not allowed.'
+    message = "Requests not allowed."
 
     def has_permission(self, request, view):
-    	auth = get_authorization_header(request).split()
-    	print('auth', auth)
-    	return True
+        auth = get_authorization_header(request).split()
+        print("auth", auth)
+        return True
--- rest_example/applications/views.py
+++ rest_example/applications/views.py
@@ -1,11 +1,14 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.mixins import GenericViewSet
+from rest_framework.decorators import action
+from rest_framework.authentication import get_authorization_header
+from rest_framework.response import Response
 
 from rest_example.applications.models import Application
 from rest_example.applications.serializers import ApplicationSerializer
-from rest_example.applications.permissions import ApplicationAPIKeyPermission 
+from rest_example.applications.permissions import ApplicationAPIKeyPermission
 
 
-class ApplicationViewSet(ReadOnlyModelViewSet):
+class ApplicationViewSet(GenericViewSet):
     """
     A simple ViewSet for viewing Application's.
     """
@@ -13,3 +16,11 @@ class ApplicationViewSet(ReadOnlyModelViewSet):
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
     permission_classes = [ApplicationAPIKeyPermission]
+
+    @action(detail=False, methods=['get'])
+    def test(self, request, pk=None):
+    	authorization_header = get_authorization_header(request).split()
+    	return Response({})
+
+
+
