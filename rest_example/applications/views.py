7ab4df3692788f1db467e9e29577c9c48e7975ac
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 03:05:03 +0400

    Created ApplicationViewSet

--- rest_example/applications/views.py
+++ rest_example/applications/views.py
@@ -1,1 +1,12 @@
-class Application
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from rest_example.applications.models import Application
+from rest_example.applications.serializers import ApplicationSerializer
+
+
+class ApplicationViewSet():
+    """
+    A simple ViewSet for viewing Application's.
+    """
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
