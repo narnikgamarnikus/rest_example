2aaa621a252304d27b00c1a5304dadbeb0cf7be4
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 01:57:26 +0400

    Added application fixture to conftest.py

--- rest_example/conftest.py
+++ rest_example/conftest.py
@@ -4,16 +4,21 @@ from django.test import RequestFactory
 
 from rest_example.users.tests.factories import UserFactory
 
+from rest_example.applications.tests.factories import ApplicationFactory
+from rest_example.applications.models import Application
+
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
 
-
 @pytest.fixture
 def user() -> settings.AUTH_USER_MODEL:
     return UserFactory()
 
+@pytest.fixture
+def application() -> Application:
+    return ApplicationFactory()
 
 @pytest.fixture
 def request_factory() -> RequestFactory:
