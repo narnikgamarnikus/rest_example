ea75aaf45f934e8239273d1455ea147e42997968
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 14:20:41 +0400

    Set api_key as ReadOnlyField to ApplicationSerializer

--- rest_example/applications/serializers.py
+++ rest_example/applications/serializers.py
@@ -1,9 +1,11 @@
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework.serializers import ModelSerializer, ReadOnlyField
 from rest_example.applications.models import Application
 
 
 class ApplicationSerializer(ModelSerializer):
+
+    api_key = ReadOnlyField()
+
     class Meta:
         model = Application
         fields = "__all__"
