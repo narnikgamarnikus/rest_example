5b82e35f92720f90708c64d711b068bb84b68c4f
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 13:48:22 +0400

    Removed test action from ApplicationViewSet

--- rest_example/applications/views.py
+++ rest_example/applications/views.py
@@ -1,7 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from rest_example.applications.models import Application
 from rest_example.applications.serializers import ApplicationSerializer
@@ -19,11 +17,3 @@ class ApplicationViewSet(ModelViewSet):
     serializer_class = ApplicationSerializer
     permission_classes = [ApplicationAPIKeyPermission]
 
-    @action(detail=False, methods=["get"])
-    def test(self, request):
-        api_key = request.META.get("api_key", None)
-        instance = get_object_or_None(Application, api_key=api_key)
-        if instance:
-            serializer = self.get_serializer(instance)
-            return Response(serializer.data, status=HTTP_200_OK)
-        return Response({"message": "Not found"}, status=HTTP_404_NOT_FOUND)
