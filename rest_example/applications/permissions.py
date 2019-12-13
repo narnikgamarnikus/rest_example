f3d69e389333b1878444bb33991b13ccb29dc3e3
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 13:54:12 +0400

    Added checking permissions to has_permission in ApplicationAPIKeyPermission

--- rest_example/applications/permissions.py
+++ rest_example/applications/permissions.py
@@ -1,9 +1,13 @@
 from rest_framework.permissions import BasePermission
 
+from rest_example.applications.models import Application
+
+from annoying.functions import get_object_or_None
+
 
 class ApplicationAPIKeyPermission(BasePermission):
     message = "Requests not allowed."
 
-    def has_permission(self, request, view):
-    	# TODO: Implement checking permissions
-        return True
+    def has_permission(self, request):
+    	api_key = request.META.get('HTTP_API_KEY', None)
+        return bool(get_object_or_None(Application, api_key=api_key))
