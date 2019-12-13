1660d0ccd97098484db7dfd025ac984b810db0ab
narnik <vladimirmyshkovski@gmail.com>
2019-12-13 14:50:55 +0400

    Changed LOGIN_REDIRECT_URL on settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -94,7 +94,7 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "users:redirect"
+LOGIN_REDIRECT_URL = "rest_login"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
@@ -295,7 +295,7 @@ CORS_ORIGIN_WHITELIST = (
 # django-rest-framework
 # ------------------------------------------------------------------------------
 # https://github.com/encode/django-rest-framework
-REST_FRAMEWORK = {auth
+REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
